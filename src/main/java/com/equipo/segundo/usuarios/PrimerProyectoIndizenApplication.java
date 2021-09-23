c28d808028a05d98878dd92deaedf7c523775c6f
HectorRealpe <hl.realpe@indizen.com>
2021-09-23 09:19:31 +0200

    Primer Commit

---
+++ src/main/java/com/equipo/segundo/usuarios/PrimerProyectoIndizenApplication.java
@@ -0,0 +1,13 @@
+package com.equipo.segundo.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerProyectoIndizenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerProyectoIndizenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equipo/segundo/usuarios/PrimerProyectoIndizenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo.segundo.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerProyectoIndizenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
