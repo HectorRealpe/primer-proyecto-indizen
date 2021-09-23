6c015dd58811fb9e56a72dc706fb16d9384de1fa
HectorRealpe <hl.realpe@indizen.com>
2021-09-23 10:10:11 +0200

    router-controlador y vista básicos creados

---
+++ src/main/java/com/equipo/segundo/usuarios/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.equipo.segundo.usuarios.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	@GetMapping({"/index", "/", "/home"})
+	public String index() {
+		
+		return "index";
+		
+	}
+	
+}
