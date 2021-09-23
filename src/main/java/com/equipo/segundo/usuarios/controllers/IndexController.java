53d37c6c91af0092d9a7a18415b7aa4e01b0da21
HectorRealpe <hl.realpe@indizen.com>
2021-09-23 10:16:33 +0200

    testeando modelo que permite pasar datos del controlador a la vista

--- src/main/java/com/equipo/segundo/usuarios/controllers/IndexController.java
+++ src/main/java/com/equipo/segundo/usuarios/controllers/IndexController.java
@@ -1,14 +1,15 @@
 package com.equipo.segundo.usuarios.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
 	@GetMapping({"/index", "/", "/home"})
-	public String index() {
-		
+	public String index(Model model) {
+		model.addAttribute("titulo", "Testeando pasar datos desde el controlador a la vista");
 		return "index";
 		
 	}
