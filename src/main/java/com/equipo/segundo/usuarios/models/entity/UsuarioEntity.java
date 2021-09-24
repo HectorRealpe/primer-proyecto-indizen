282938f668ba433cf0a0f81eaac9494dbc01d5cf
HectorRealpe <hl.realpe@indizen.com>
2021-09-24 17:57:35 +0200

    primer commit

---
+++ src/main/java/com/equipo/segundo/usuarios/models/entity/UsuarioEntity.java
@@ -0,0 +1,85 @@
+package com.equipo.segundo.usuarios.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="usuarios")
+public class UsuarioEntity implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private String nombre;
+	private String apellido;
+	private String nick;
+	private String sexo;
+	private String email;
+	private int edad;
+	private String banco;
+	private boolean pelis;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getBanco() {
+		return banco;
+	}
+	public void setBanco(String banco) {
+		this.banco = banco;
+	}
+	public boolean isPelis() {
+		return pelis;
+	}
+	public void setPelis(boolean pelis) {
+		this.pelis = pelis;
+	}
+	
+	private static final long serialVersionUID = 1L;
+	
+}
