033f2be72fea1ac64bbef7f8bb7fe75d50486ead
HectorRealpe <hl.realpe@indizen.com>
2021-09-27 08:45:48 +0200

    todo correcto y funcionando -structuralmente-

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO usuarios (id, nombre, apellido, nick, sexo, email, edad, banco, pelis) VALUES (1, 'Héctor', 'Realpe', 'helui', 'masculino', 'hl.realpe@indizen.com', 38, 'BASD6464567', false);
+INSERT INTO usuarios (id, nombre, apellido, nick, sexo, email, edad, banco, pelis) VALUES (2, 'nombre2', 'apellido2', 'nickPrueba', 'femenino', 'prueba@indizen.com', 32, 'BASD6464567', false);
