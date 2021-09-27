9bf1874fa0ab76cd0545674fc8a7d504016ca580
HectorRealpe <hl.realpe@indizen.com>
2021-09-27 08:48:49 +0200

    último commit antes del examen -versión para master-

--- src/main/java/com/equipo/segundo/usuarios/models/Usuario.java
+++ src/main/java/com/equipo/segundo/usuarios/models/Usuario.java
@@ -22,21 +22,6 @@ public class Usuario implements Serializable {
 	private int edad;
 	private String banco;
 	private boolean pelis;	
-	
-	
-	
-	public Usuario(long id, String nombre, String apellido, String nick, String sexo, String email, int edad,
-			String banco, boolean pelis) {
-		this.id = id;
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.nick = nick;
-		this.sexo = sexo;
-		this.email = email;
-		this.edad = edad;
-		this.banco = banco;
-		this.pelis = pelis;
-	}
 
 	public long getId() {
 		return id;
