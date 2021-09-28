664064da286f39b27cdc9a32e91c26590a107c20
HectorRealpe <hl.realpe@indizen.com>
2021-09-28 12:24:10 +0200

    fail 2

--- src/main/java/com/equipo/segundo/usuarios/controllers/IndexController.java
+++ src/main/java/com/equipo/segundo/usuarios/controllers/IndexController.java
@@ -1,11 +1,15 @@
 package com.equipo.segundo.usuarios.controllers;
 
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.equipo.segundo.usuarios.models.InterfaceCRUD;
 import com.equipo.segundo.usuarios.models.Usuario;
@@ -20,7 +24,10 @@ public class IndexController {
 
 	// Routing inicial y paso de datos del controlador a la vista para un HTML muy básico
 	@GetMapping({"/index","/", "/home"})
-	public String index(Model model) {	
+	public String index(@RequestParam(value="error", required=false) String error,
+			@RequestParam(value="logout", required = false) String logout,
+			Model model, Principal principal, RedirectAttributes flash) {
+		
 		model.addAttribute("titulo", "Video Club");
 		model.addAttribute("welcome", "Bienvenido");
 		model.addAttribute("login", "Iniciar Sesion");
