d36a67cacc1eed3e81facc638c02b79f31f6db1b
HectorRealpe <hl.realpe@indizen.com>
2021-09-28 13:18:48 +0200

    seguridad funcionando, avanzando

---
+++ src/main/java/com/equipo/segundo/usuarios/SpringSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.equipo.segundo.usuarios;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    	
+        auth.inMemoryAuthentication()
+	        .withUser("admin")
+	        	.password("{noop}pass")
+	        		.roles("ADMIN", "USER")
+	        	.and()
+	            .withUser("{noop}pass")
+		        	.password("pass")
+		        		.roles("USER")
+		        .and()
+		        .withUser("javier")
+		        	.password("{noop}pass")
+		        		.roles("USER")
+		        .and()
+		        .withUser("hector")
+		        	.password("{noop}pass")
+		        		.roles("USER");
+        
+	}
+	
+}
