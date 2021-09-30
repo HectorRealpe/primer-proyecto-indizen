6616595f10ebae84be9ee726fe07eb7f12c8e5ae
Javier Linares Gómez <j.linares@indizen.com.es>
2021-09-30 16:57:17 +0200

    actual

---
+++ src/main/resources/static/Js/anadirUsuario.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+	let input = $('#inputPersona').DataTable({
+		"ajax": {
+			"url": "http://localhost:8080/api/usuarios",
+			"type": "POST",
+			"dataSrc": "",
+		}
+		});
+		
+		function enviarFormulario(){
+			var objeto = {
+			nombre: $( "#inputNombre" ).text(),
+			apellido: $( "#inputApellidos" ).text(),
+			nick: $( "#inputNick" ).text(),
+			sexo: $( "#inputSexo" ).text(),
+			email: $( "#inputEmail" ).text(),
+			edad: $( "#inputEdad" ).text(),
+			banco: $( "#inputBanco" ).text(),
+					
+		};
+		}
+		});
